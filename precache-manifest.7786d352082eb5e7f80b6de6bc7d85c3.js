22ddc61c6ee0959d8f18dba256a2b81abd176fcb
Junior Dallagnolo <ernilodjr@gmail.com>
2018-12-05 16:07:25 -0500

    Updates

---
+++ precache-manifest.7786d352082eb5e7f80b6de6bc7d85c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReduxTutorial/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2b36059bd76b11dcec60",
+    "url": "/ReduxTutorial/static/js/runtime~main.2b36059b.js"
+  },
+  {
+    "revision": "7e01cc352398353797b6",
+    "url": "/ReduxTutorial/static/js/main.7e01cc35.chunk.js"
+  },
+  {
+    "revision": "dc3b53c5928fbd829be7",
+    "url": "/ReduxTutorial/static/js/1.dc3b53c5.chunk.js"
+  },
+  {
+    "revision": "7e01cc352398353797b6",
+    "url": "/ReduxTutorial/static/css/main.a3c89a58.chunk.css"
+  },
+  {
+    "revision": "b7ef8cf0921aacf517f27a6bf1ffbe17",
+    "url": "/ReduxTutorial/index.html"
+  }
+];
